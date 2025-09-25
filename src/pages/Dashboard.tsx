5d207a074d4806fe4972e3ac6008f12e8aa2190f
sree <sreeraj_ec@hotmail.com>
2025-09-25 08:09:40 +0800

    EXPLORE-3-Setup-Frontend-Application: Router setup completed
    React Router DOM setup complete

---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,5 @@
+const Dashboard = () => {
+  return <div className="p-4 text-xl">This is your dashboard 🧭</div>;
+};
+
+export default Dashboard;
---
+++ src/pages/LandingPage.tsx
@@ -0,0 +1,5 @@
+const LandingPage = () => {
+  return <div className="p-4 text-xl">Welcome to XploreSG 🚲</div>;
+};
+
+export default LandingPage;
